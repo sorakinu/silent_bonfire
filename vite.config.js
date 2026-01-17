6d46356c1154fd68a4e2bffdd09bf4f6aabc8002
sorakinu <maroyuki728@gmail.com>
2026-01-17 20:44:11 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/silent_bonfire/', 
+  base: '/silent_bonfire/', /silent_bonfire/
 })
